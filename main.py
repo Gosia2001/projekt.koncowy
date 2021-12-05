c5512e6b05f0b1a299db165be887e88527d74587
Gosia2001 <malgorzatasierla@gmail.com>
2021-12-05 18:07:06 +0100

    a

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-print('qwerty')
-i == 1
-def funkcja(z):
-    x=4
-    y = x+4
-    print('tsdf')
+print('Podaj mase pierwszej kulki od 1 do 5 kilogramów')
+M1 = int(input())
+
+print('Podaj mase drugiej kulki od 1 do 5 kilogramów')
+M2 = int(input())
+if M1 < 1:
+    print('Podano za małą wartość')
+elif M1 > 5:
+    print('Podano za dużą wartość')
+else:
+    print('Podano masę pierwszej kulki:', M1)
+
+if M2 < 1:
+    print('Podano za małą wartość')
+elif M2 > 5:
+    print('Podano za dużą wartość')
+else:
+    print('Podano masę drugiej kulki:', M2)
+
+print('Podaj szybkość początkową pierwszej kulki w:')
+V1 = int(input())
+print('Podaj szybkość początkową drugiej kulki:')
+V2 = int(input())
+if V1 < 0:
+    print('Podano za małą prędkość')
+elif V1 > 10:
+    print( 'Podano za dużą wartość')
+else:
+    print('Podano wartość szybkości pierwszej kulki:', V1)
+if V2 < 0:
+    print('Podano za małą prędkość')
+elif V2 > 10:
+    print( 'Podano za dużą wartość')
+else:
+    print('Podano wartość szybkości drugiej kulki:', V2)
