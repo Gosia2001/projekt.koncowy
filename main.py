397b9c0b64cb6da1d4d995ddd71eb69ff9d9ba56
KatarzynaTemplin <templin856@gmail.com>
2021-12-19 21:17:49 +0100

    tydzień 3

--- main.py
+++ main.py
@@ -41,24 +41,69 @@ V2k = V1*(2*M1)/(M1+M2)+V2*(M2-M1)/(M1+M2)
 print('Wartość prędkości końcowej drugiej kulki wynosi:', V2k)
 # prędkość V1k powinna być na "-" ponieważ porusza się po osi X w przeciwnym kierunku
 
+turtle.title('Zderzenia centralna')
+
+#ściany
+sciana1 = turtle.Turtle()
+sciana1.color('black')
+sciana1.pensize(5)
+sciana1.penup()
+sciana1.goto(250,-100)
+sciana1.pendown()
+sciana1.goto(250,100)
+sciana1.hideturtle()
+
+sciana2 = turtle.Turtle()
+sciana2.color('black')
+sciana2.pensize(5)
+sciana2.penup()
+sciana2.goto(-250,100)
+sciana2.pendown()
+sciana2.goto(-250,-100)
+sciana2.hideturtle()
+
+sufit = turtle.Turtle()
+sufit.color('black')
+sufit.pensize(5)
+sufit.penup()
+sufit.goto(-250,100)
+sufit.pendown()
+sufit.goto(250,100)
+sufit.hideturtle()
+
+podloga = turtle.Turtle()
+podloga.color('black')
+podloga.pensize(5)
+podloga.penup()
+podloga.goto(-250,-100)
+podloga.pendown()
+podloga.goto(250,-100)
+podloga.hideturtle()
+
 t1 = turtle.Turtle()
 t1.shape('turtle')
 t1.color('green')
-t1.forward(100)
-t1.speed(1)
-x1, y1 = t1.position()
+t1.forward(1000)
+t1.speed(0)
+t1.width(20)
 
-t2 = turtle.Turtle()
-t2.setposition(200,0)
-t2.shape('turtle')
-t2.color('blue')
-t2.backward(500)
-t2.speed(1)
-x2, y2 = t2.position()
+if xt1.xcor() >= 250 or x <= -250:
+    t1.left(180)
 
-if ((x1) - (x2)) < 10:
-    t1.backward()
-    t2.forward()
+
+
+#t2 = turtle.Turtle()
+#t2.setposition(200,0)
+#t2.shape('turtle')
+#t2.color('blue')
+#t2.backward(200)
+#t2.speed(V2)
+
+#if t2.position == t1.position:
+#    t1.backward(100)
+ #   t1.speed(V1k)
+  #  t2.forward(100)
+   # t2.speed(V2k)
 
 screen = turtle.getscreen()
 turtle.exitonclick()
